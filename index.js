fc6134303a1c41cc3e4e95cce85e7f85999e4fe8
An <123456@gmail.com>
2018-11-16 23:29:21 +0700

    Improve cat eat mothod

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+var Cat=require('./cat');
+var Mouse=require('./mouse');
 var Dog=require('./dog1');
+
+var cat=new Cat();
+var mouse=new Mouse('Mickey');
+var dog=new Dog();
+try{
+	cat.eat(mouse);
+}catch(error){
+	console.log('error while cat eat animal # mouse');
+}
+console.log(cat);
